220400a6af7d6bab6dda2a15fffe4ce26d757b06
yanela <yannw7e@gmail.com>
2025-11-08 22:55:16 -0500

    Subo ejercicio propuesto Semana 12

---
+++ S12_Ejercicio/Libreria.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Laboratorio_S12
+{
+    public class Libreria
+    {
+        private static string[] Nombres = new string[0];
+        private static double[] Precios = new double[0];
+        private static int Contador = 0;
+
+        public static void Registrar()
+        {
+            //PEDIR NOMBRE
+            string NombreLibro = "";
+            Console.WriteLine("\n- REGISTRAR LIBRO -");
+            bool huboError;
+
+            do
+            {
+                huboError = false;
+
+                Console.Write("\nNombre del libro: ");
+                NombreLibro = Console.ReadLine();
+
+                if (NombreLibro == null || NombreLibro == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n¡ERROR! El nombre no puede estar vacío.");
+                    Console.WriteLine("Inténtelo nuevamente . . .");
+                    Console.ResetColor();
+                    huboError = true;
+                }
+                else
+                {
+                    for (int i = 0; i < Contador; i++)
+                    {
+                        if (NombreLibro.ToLower() == Nombres[i].ToLower())
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("\n¡ERROR! El libro ya existe.");
+                            Console.ResetColor();
+                            huboError = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            while (huboError);
+
+            //PEDIR PRECIO
+            string PrecioTexto;
+            double Precio = 0;
+            Console.WriteLine("\n");
+            Console.WriteLine("- REGISTRAR PRECIO -");
+
+            do
+            {
+                Console.Write("\nPrecio: ");
+                PrecioTexto = Console.ReadLine();
+
+                if (PrecioTexto == null || !double.TryParse(PrecioTexto, out Precio))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n¡ERROR! Precio debe ser un número válido.");
+                    Console.WriteLine("Inténtelo nuevamente . . .");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    if (Precio < 0 || Precio > 1000)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("\n¡ERROR! Precio debe ser entre 0 y 1000.");
+                        Console.WriteLine("Inténtelo nuevamente . . .");
+                        Console.ResetColor();
+                    }
+                }
+            }
+            while (PrecioTexto == null || !double.TryParse(PrecioTexto, out Precio) || (Precio < 0 || Precio > 1000));
+
+
+            Array.Resize(ref Nombres, Contador + 1);
+            Array.Resize(ref Precios, Contador + 1);
+
+            Nombres[Contador] = NombreLibro;
+            Precios[Contador] = Precio;
+            Contador++;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nLibro registrado exitosamente.");
+            Console.WriteLine("Presione una tecla para volver a la cinta de opciones.");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        public static void Mostrar()
+        {
+            Console.WriteLine("\n- LIBROS REGISTRADOS: " + Contador + " -\n");
+
+            if (Contador == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("No hay libros registrados.");
+                Console.ResetColor();
+            }
+            else
+            {
+                for (int i = 0; i < Contador; i++)
+                {
+                    Console.WriteLine((i + 1) + ". Nombre: " + (Nombres[i]) + ", Precio: " + (Precios[i]) + "\n");
+
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Presione una tecla para volver a la cinta de opciones.");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        public static void Modificar()
+        {
+            string NombreBuscar;
+            bool huboError;
+            int indice = -1;
+            Console.WriteLine("\n- MODIFICAR LIBRO -");
+            do
+            {
+                huboError = false;
+                Console.Write("\nNombre del libro a modificar: ");
+                NombreBuscar = Console.ReadLine();
+                indice = -1;
+                for (int i = 0; i < Contador; i++)
+                {
+                    if (Nombres[i].ToLower() == NombreBuscar.ToLower())
+                    {
+                        indice = i;
+                        break;
+                    }
+                }
+                if (indice == -1)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n¡ERROR! Libro no encontrado.");
+                    Console.WriteLine("Inténtelo nuevamente . . .");
+                    Console.ResetColor();
+                    huboError = true;
+                }
+            }
+            while (huboError);
+
+            string NuevoNombre;
+            do
+            {
+                huboError = false;
+                Console.Write("\nIngrese el nuevo nombre: ");
+                NuevoNombre = Console.ReadLine();
+                if (NuevoNombre == null || NuevoNombre == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n¡ERROR! El nombre no puede estar vacío.");
+                    Console.WriteLine("Inténtelo nuevamente . . .");
+                    Console.ResetColor();
+                    huboError = true;
+                }
+                else
+                {
+                    for (int i = 0; i < Contador; i++)
+                    {
+                        if (i != indice && Nombres[i].ToLower() == NuevoNombre.ToLower())
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("\n¡ERROR! El nuevo nombre ya existe.");
+                            Console.WriteLine("Inténtelo nuevamente . . .");
+                            Console.ResetColor();
+                            huboError = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            while (huboError);
+            string PrecioTexto;
+            double NuevoPrecio = 0;
+            do
+            {
+                Console.Write("\nIngrese el nuevo precio: ");
+                PrecioTexto = Console.ReadLine();
+                if (PrecioTexto == null || !double.TryParse(PrecioTexto, out NuevoPrecio))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n¡ERROR! Precio debe ser un número válido.");
+                    Console.WriteLine("Inténtelo nuevamente . . .");
+                    Console.ResetColor();
+                }
+                else if (NuevoPrecio < 0 || NuevoPrecio > 1000)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n¡ERROR! Precio debe ser entre 0 y 1000.");
+                    Console.WriteLine("Inténtelo nuevamente . . .");
+                    Console.ResetColor();
+                }
+            }
+            while (PrecioTexto == null || !double.TryParse(PrecioTexto, out NuevoPrecio) || (NuevoPrecio < 0 || NuevoPrecio > 1000));
+            Nombres[indice] = NuevoNombre;
+            Precios[indice] = NuevoPrecio;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nLibro modificado exitosamente.");
+            Console.WriteLine("Presione una tecla para volver a la cinta de opciones.");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        public static void Eliminar()
+        {
+            string NombreBuscar;
+            bool huboError;
+            int indice = -1;
+
+            Console.WriteLine("\n- ELIMINAR LIBRO -");
+            do
+            {
+                huboError = false;
+                Console.Write("\nNombre del libro a eliminar: ");
+                NombreBuscar = Console.ReadLine();
+                indice = -1;
+                for (int i = 0; i < Contador; i++)
+                {
+                    if (Nombres[i].ToLower() == NombreBuscar.ToLower())
+                    {
+                        indice = i;
+                        break;
+                    }
+                }
+                if (indice == -1)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n¡ERROR! Libro no encontrado.");
+                    Console.WriteLine("Inténtelo nuevamente . . .");
+                    Console.ResetColor();
+                    huboError = true;
+                }
+            }
+            while (huboError);
+
+            for (int i = indice; i < Contador - 1; i++)
+            {
+                Nombres[i] = Nombres[i + 1];
+                Precios[i] = Precios[i + 1];
+            }
+            Array.Resize(ref Nombres, Contador - 1);
+            Array.Resize(ref Precios, Contador - 1);
+            Contador--;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nLibro eliminado exitosamente.");
+            Console.WriteLine("Presione una tecla para volver a la cinta de opciones.");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ S12_Ejercicio/Menu.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Laboratorio_S12
+{
+    internal class Menu
+    {
+        static void DibujarEncabezado(int opcionSeleccionada)
+        {
+            string[] arregloMenu = { "  REGISTRAR  ", "  MOSTRAR  ", "  MODIFICAR  ", "  ELIMINAR  ", "  SALIR  " };
+
+            Console.Clear();
+
+            Console.WriteLine("*--------------------------------------------------------*");
+            Console.WriteLine("|                                                        |");
+            Console.WriteLine("|                BIENVENIDO A LA LIBRERÍA                |");
+            Console.WriteLine("|                                                        |");
+            Console.WriteLine("*--------------------------------------------------------*");
+
+            for (int i = 0; i < arregloMenu.Length; i++)
+            {
+                if (i == opcionSeleccionada)
+                {
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(arregloMenu[i]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write(arregloMenu[i]);
+                }
+            }
+            Console.WriteLine("\n*--------------------------------------------------------*");
+        }
+
+        public static void Main(string[] args)
+        {
+            int Opc;
+            do
+            {
+                Opc = MenuInteractivo();
+                DibujarEncabezado(Opc);
+
+                switch (Opc)
+                {
+                    case 0:
+                        Libreria.Registrar();
+                        break;
+
+                    case 1:
+                        Libreria.Mostrar();
+                        break;
+
+                    case 2:
+                        Libreria.Modificar();
+                        break;
+
+                    case 3:
+                        Libreria.Eliminar();
+                        break;
+
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("\nCerrando programa . . .");
+                        Console.WriteLine("¡Hasta luego! :)");
+                        Console.ResetColor();
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            while (Opc != 4);
+
+        }
+
+        public static int MenuInteractivo()
+        {
+            string[] arregloMenu = { "  REGISTRAR  ", "  MOSTRAR  ", "  MODIFICAR  ", "  ELIMINAR  ", "  SALIR  " };
+
+            int index = 0;
+            ConsoleKey tecla;
+
+            do
+            {
+                DibujarEncabezado(index);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\nUse las flechas <- -> para navegar.");
+                Console.WriteLine("\nPresione ENTER para seleccionar una opción.");
+                Console.ResetColor();
+
+                ConsoleKeyInfo info = Console.ReadKey(true);
+                tecla = info.Key;
+
+                if (tecla == ConsoleKey.RightArrow)
+                {
+                    index++;
+                    if (index > arregloMenu.Length - 1) index = 0;
+                }
+                else if (tecla == ConsoleKey.LeftArrow)
+                {
+                    index--;
+                    if (index < 0) index = arregloMenu.Length - 1;
+                }
+            } while (tecla != ConsoleKey.Enter);
+
+            return index;
+        }
+    }
+}
