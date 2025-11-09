d65156f996bc7005c36d2e5c563936a3ab55b33f
yanela <yannw7e@gmail.com>
2025-11-08 22:40:31 -0500

    Subo datos personales

---
+++ Datos_Personales/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos_Personales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombres: Yanela Liz");
+            Console.WriteLine("Apellidos: Acori Sanchez");
+        }
+    }
+}
